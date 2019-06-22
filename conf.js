2905386343fe46462e5e14cfecfe1676513aec4c
daniel kontos <kontos.daniel@gmail.com>
2019-06-23 01:18:11 +0300

    Added new test in the conf file.

--- conf.js
+++ conf.js
@@ -1,4 +1,4 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['todo-spec.js', 'tapTests.js']
+    specs: ['todo-spec.js', 'tap-tests.js', 'delta-tests.js']
   };
